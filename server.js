5eca7f4e3c6c157f1c66f9c9d1725a479d664e3d
Jeri Fey <jericfey@gmail.com>
2020-12-13 17:41:00 -0700

    index.html file is being read correctly

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const fs = require("fs");
 const PORT = 8080;
 
 const handleRequest = (req, res) => {
-  res.end("End.");
+  // Here we use the fs package to read our index.html file
+  fs.readFile(`${__dirname}/public/index.html`, (err, data) => {
+    if (err) throw err;
+    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering an html file.
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
 };
 
 const server = http.createServer(handleRequest);
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on Port: ${PORT}`);
 });
